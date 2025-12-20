281ced2ff4b0556fdb6dc9533c8193089204a809
Patsagorn <patsagorn_y@outlook.com>
2025-12-20 19:16:14 +0700

    Comment out Block module import in Twinkle

--- src/twinkle.ts
+++ src/twinkle.ts
@@ -10,7 +10,7 @@ import { DiffCore as Diff } from './core';
 
 // register some globals for debugging, as per twinkle v2
 import './globals';
-import { Block } from './block';
+// import { Block } from './block';
 import { BatchDelete } from './batchdelete';
 import { Unlink } from './unlink';
 import { Protect } from './protect';
